eceda8e2da489404a304a761f5cf67e45ea7b26b
noam952 <70768579+noam952@users.noreply.github.com>
2021-08-28 15:09:48 +0300

    Tested More Features

--- spec/index.js
+++ spec/index.js
@@ -8,4 +8,8 @@ db.set("MyData", {
 const ReaverUtil = new Util("./json.sqlite");
 console.log(ReaverUtil.parseDataKey("pokecoins_38502357802385_392509257809235"))
 
+db.set("Data2", {
+    1: { Name: "AI", Ability: "To autocomplete code!" }
+});
+
 // db.'method'("key");
